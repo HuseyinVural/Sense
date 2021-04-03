ec9fbeee827cf394d6db5bba4f2fbe0bfa755b1b
Huseyin VURAL <huseyin.vural@getir.com>
2021-04-03 14:16:33 +0300

    Initial Commit

---
+++ Sense/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sense
+//
+//  Created by Hüseyin Vural on 3.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
